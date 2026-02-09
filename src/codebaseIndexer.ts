6a7953aaec89659b5e0380de7eef881cc7a90fe2
Yash Bhalchandra <yashb1004@gmail.com>
2026-02-08 19:45:31 -0500

    design for how this will work approx

---
+++ src/codebaseIndexer.ts
@@ -0,0 +1,14 @@
+// import project
+// import path in
+
+//one interface for interfaces
+
+//one interface for classes
+
+//one combination interface for what we send to gpt/any other ai
+
+// main method that gets the classes, gets the interfaces, pushes them onto the interfaces and then gets everything ready to send
+
+// method to get classes
+
+// method to get interfaces
